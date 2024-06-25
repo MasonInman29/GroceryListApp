c0fd3a2c95fd04c68caa2a77134d9d451d717e41
Mason Inman <76747556+MasonInman29@users.noreply.github.com>
2024-06-24 19:27:19 -0500

    initial commit

--- app/modal.tsx
+++ app/modal.tsx
@@ -7,7 +7,8 @@ import { Text, View } from '@/components/Themed';
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Text style={styles.title}>By: Mason Inman</Text>
+      <Text style={styles.p}>Why? Well in true engineer fashion why take 5 seconds to write your grocery list on a sticky note when you can spend hours to make a computer can do it for you?</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/modal.tsx" />
 
@@ -23,13 +24,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  p: {
+    fontSize: 16,
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    paddingLeft: 20,
+    paddingRight: 20,
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
   },
+  
 });
